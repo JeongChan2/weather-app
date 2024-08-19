fd7f4348943fbc57b0a6f2663a1809734a25771c
JeongChan2 <jcstar55@gmail.com>
2024-08-19 23:41:47 +0900

    refactor: clear NaN

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const WeatherBox = ({weather}) => {
   // console.log(weather)
-  return (
+  return (weather?
     <div className='weather-box'>
-      <div>{weather?.name}</div>
-      <h2>{weather?.main.temp}C / { (9/5*weather?.main.temp + 32).toFixed(2)}F</h2>
-      <h3>{weather?.weather[0].description}</h3>
+      <div>{weather.name}</div>
+      <h2>{weather.main.temp}C / { (9/5*weather.main.temp + 32).toFixed(2)}F</h2>
+      <h3>{weather.weather[0].description}</h3>
     </div>
-  )
+  :<></>)
 }
 
 export default WeatherBox
